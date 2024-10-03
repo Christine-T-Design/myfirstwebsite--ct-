f5c50c2d4295cf873a93ef9f4a0ef38c4ffed1ea
Christine-T-Design <thammasc28@gmail.com>
2024-10-03 11:48:27 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -20,13 +20,13 @@ let y; // this is a declaration;
 let age = 25;
 let price = 10.99;
 let gpa = 2.1;
-console.log("You are ${"age"} years old");
-console.log("The price is ${"price"}");
-console.log("Your gpa is${"gpa"}");
+console.log("You are ${age} years old");
+console.log("The price is ${price}");
+console.log("Your gpa is${gpa}");
 
 let firstName = "Bro"; //why is this a string;
 console.log(typeof firstName);
-console.log("Your name is ${"firstName"}");
+console.log("Your name is ${firstName}");
 //strings can include numbers, but we can't use those numbers in math;
 
 //booleans are either true or false;
