72477e9bb818f5fc0e2da7a968d4fc20e61f3fc8
Christine-T-Design <thammasc28@gmail.com>
2024-10-03 12:00:05 -0700

    changing the let... to comments and trying to figure out firstName command

--- js/scripts.js
+++ js/scripts.js
@@ -17,16 +17,15 @@ let y; // this is a declaration;
 
 
 
-let age = 25;
-let price = 10.99;
-let gpa = 2.1;
+//let age = 25;
+//let price = 10.99;
+//let gpa = 2.1;
 console.log(`You are ${age}`);
 console.log(`The price is ${price}`);
 console.log(`Your gpa is${gpa}`);
 
-let firstName = "Bro"; //why is this a string;
+let firstName = "Bro"; 
+//why is this a string;
 console.log(typeof firstName);
-console.log(`Your name is ${firstName}');
-//strings can include numbers, but we can't use those numbers in math;
+console.log(`firstName`);
 
-//booleans are either true or false;
