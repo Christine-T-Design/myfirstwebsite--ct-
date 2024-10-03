f0ca95e0fa2299cc91be0c461f0dc4f41a37c9c4
Christine-T-Design <thammasc28@gmail.com>
2024-10-03 11:52:13 -0700

    added [``] to see if they made sentences

--- js/scripts.js
+++ js/scripts.js
@@ -20,9 +20,9 @@ let y; // this is a declaration;
 let age = 25;
 let price = 10.99;
 let gpa = 2.1;
-console.log("You are ${age} years old");
-console.log("The price is ${price}");
-console.log("Your gpa is${gpa}");
+console.log(`You are ${age}`);
+console.log(`The price is ${price}`);
+console.log(`Your gpa is${gpa}`);
 
 let firstName = "Bro"; //why is this a string;
 console.log(typeof firstName);
