3d7c5941ac102017c6a3a7e9878eed532254b600
Christine-T-Design <thammasc28@gmail.com>
2024-10-03 11:26:58 -0700

    learning JS through a youtube video

--- js/scripts.js
+++ js/scripts.js
@@ -10,4 +10,18 @@ window.alert("We're out of dumplings!");
 this 
 is 
 a 
-comment 
+comment */
+
+let x; //this is a declaration 
+let y; // this is a declaration 
+
+x=100 
+
+let x=123; //this is both a declaration and an assignment 
+
+let age = 25;
+let price = 10.99;
+let gpa = 2.1;
+console.log("You are ${age} years old");
+console.log("The price is ${price}");
+console.log("Your gpa is${gpa}")
