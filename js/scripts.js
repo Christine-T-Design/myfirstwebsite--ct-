484ad96b2c235bb9c2b4ed02a774108df03eebaf
Christine-T-Design <thammasc28@gmail.com>
2024-10-02 23:02:19 -0700

    adding play code in script.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
-console.log("Hello Everyone!");
+console.log("Ni Haody!");
+console.log ("I like dumplings");
+
+window.alert("this is an alert!");
+window.alert("We're out of dumplings!");
+
+// this is a comment 
+
+/*
+this 
+is 
+a 
+comment 
